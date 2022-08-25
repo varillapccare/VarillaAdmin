009e44dc455c2edf6aa0892537530337667807a0
varillapccare <97778774+varillapccare@users.noreply.github.com>
2022-08-25 10:50:41 +0530

    backend url change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 
 axios.defaults.headers.post["Content-Type"] = "application/json";
-axios.defaults.baseURL = "https://varillabackend.herokuapp.com/";
+axios.defaults.baseURL = "https://varillapccareapi.herokuapp.com/";
 
 const App = () => (
   <BrowserRouter>
